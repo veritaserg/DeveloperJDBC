6e9631ea94c84230ece3b6b47eb6038822956190
veritaserg <infophotomasterscomua@gmail.com>
2018-11-04 00:41:47 +0200

    save

--- src/main/java/dao/jdbc/JdbcAccountDAOImpl.java
+++ src/main/java/dao/jdbc/JdbcAccountDAOImpl.java
@@ -12,9 +12,9 @@ import java.util.List;
 public class JdbcAccountDAOImpl implements AccountDAO {
     public static void main(String[] args) {
         JdbcAccountDAOImpl jdbcAccountDAO = new JdbcAccountDAOImpl();
-        Account account = new Account((long) 1,"qwer", (long)4);
-        jdbcAccountDAO.delete(2l);
-        jdbcAccountDAO.delete(3l);
+        Account account = new Account("xx", (long)44);
+   jdbcAccountDAO.update(account);
+        System.out.println(account.getDeveloperID());
 
 
     }
@@ -28,7 +28,7 @@ public class JdbcAccountDAOImpl implements AccountDAO {
             try (PreparedStatement statement = connection.prepareStatement(SQL)) {
                 statement.setString(1, account.getData());
                 statement.setLong(2, account.getId());
-                statement.executeUpdate();
+                              statement.executeUpdate();
                             connection.commit();
             }
 
@@ -44,8 +44,26 @@ public class JdbcAccountDAOImpl implements AccountDAO {
     }
 
     public void update(Account account) {
+        Connection connection = null;
 
-    }
+        String SQL = "UPDATE accounts SET name = ?,developer_id = ? WHERE developer_id = ?";
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+                statement.setLong(3, account.getDeveloperID());
+                statement.setString(1, account.getData());
+                statement.setLong(2, account.getDeveloperID());
+             statement.executeUpdate();
+             connection.commit();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Util.disconnect();
+        }
+        }
 
     public void delete(Long id) {
         String SQL = "DELETE FROM accounts WHERE developer_id = ?";
--- src/main/java/dao/jdbc/JdbcDeveloperDAOImpl.java
+++ src/main/java/dao/jdbc/JdbcDeveloperDAOImpl.java
@@ -11,48 +11,47 @@ import java.util.*;
 
 public class JdbcDeveloperDAOImpl implements DeveloperDAO {
     JdbcAccountDAOImpl jdbcAccountDAO;
-public JdbcDeveloperDAOImpl(){
-    jdbcAccountDAO = new JdbcAccountDAOImpl();
-}
+    Connection connection;
+
+    public JdbcDeveloperDAOImpl() {
+        jdbcAccountDAO = new JdbcAccountDAOImpl();
+    }
+
     public static void main(String[] args) {
         JdbcDeveloperDAOImpl jdbcDeveloperDAO = new JdbcDeveloperDAOImpl();
         Set<Skill> skills = new HashSet<>();
         skills.add(new Skill((long) 1, "qwqw"));
-        skills.add(new Skill((long) 2, "qwqw"));
-        Developer developer = new Developer((long) 100, "Petrovich", 3333L, skills, new Account("gitPetrovich", 1l));
+        //skills.add(new Skill((long) 2, "qwqw"));
+        Developer developer = new Developer((long) 44, "serg", 555L, skills, new Account("serg", 44L));
 
-      //  jdbcDeveloperDAO.create(developer);
-      jdbcDeveloperDAO.delete(2l);
+//jdbcDeveloperDAO.create(developer);
 
+        // jdbcDeveloperDAO.delete((long) 38);
+        jdbcDeveloperDAO.update(developer);
 
     }
 
     public void create(Developer developer) {
-        long id ;
+        long id;
         String SQL = "INSERT INTO developers(name,salary) VALUES(?,?)";
-        String GET_LAST_INSERTED = "SELECT LAST_INSERT_ID()";
-        Connection connection = null;
+
         try {
             connection = Util.getConnection();
             connection.setAutoCommit(false);
-
             try (PreparedStatement statement = connection.prepareStatement(SQL)) {
                 statement.setString(1, developer.getName());
                 statement.setLong(2, developer.getSalary());
                 statement.executeUpdate();
             }
-
-
             try (Statement statement = connection.createStatement()) {
                 ResultSet resultSet = statement.executeQuery("SELECT LAST_INSERT_ID()");
                 resultSet.next();
                 id = resultSet.getLong(1);
-                            Account account = developer.getAccount();
+                Account account = developer.getAccount();
                 account.setId(id);
-                System.out.println(account.getId());
+
                 jdbcAccountDAO.create(account);
             }
-
             try (PreparedStatement statement = connection.prepareStatement("INSERT developers_skills VALUES (?,?)")) {
                 for (Skill skill : developer.getSkills()) {
                     statement.setLong(1, id);
@@ -60,6 +59,7 @@ public JdbcDeveloperDAOImpl(){
                     statement.addBatch();
                 }
                 statement.executeBatch();
+
                 connection.commit();
             }
         } catch (SQLException e) {
@@ -67,7 +67,6 @@ public JdbcDeveloperDAOImpl(){
         } finally {
             Util.disconnect();
         }
-
     }
 
     public Developer getById(Long id) {
@@ -75,13 +74,49 @@ public JdbcDeveloperDAOImpl(){
     }
 
     public void update(Developer developer) {
+        String SQL = "UPDATE developers SET name = ?,salary =? WHERE ID = ?";
+        String DELETE_SKILLS = "DELETE FROM developers_skills WHERE developers_id = ?";
+
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+                statement.setString(1, developer.getName());
+                statement.setLong(2, developer.getSalary());
+                statement.setLong(3, developer.getId());
+                statement.executeUpdate();
+            }
+
+            try (PreparedStatement statement = connection.prepareStatement(DELETE_SKILLS)) {
+                statement.setLong(1, developer.getId());
+                statement.executeUpdate();
+
+            }
+
+            try (PreparedStatement statement = connection.prepareStatement("INSERT developers_skills VALUES (?,?)")) {
+                for (Skill skill : developer.getSkills()) {
+                    statement.setLong(1, developer.getId());
+                    statement.setLong(2, skill.getId());
+                    statement.addBatch();
+                }
+                statement.executeBatch();
+            }
+            connection.commit();
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        } finally {
+            Util.disconnect();
+
+        }
+        jdbcAccountDAO.update(developer.getAccount());
 
     }
 
     public void delete(Long id) {
         String SQL = "DELETE FROM developers WHERE ID = ?";
         String DELETE_SKILLS = "DELETE FROM developers_skills WHERE developers_id = ?";
-        Connection connection = null;
         try {
             connection = Util.getConnection();
             connection.setAutoCommit(false);
