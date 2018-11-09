6afdb7979230a20eb0c06a1a864ee0ca1bfdd43e
veritaserg <infophotomasterscomua@gmail.com>
2018-11-09 20:11:15 +0200

    save

--- src/main/java/dao/util/UtilAws.java
+++ src/main/java/dao/util/UtilAws.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class UtilAws {
     static final String DATABASE_URL = "mysqlaws.c05bmdvhonqe.ap-northeast-1.rds.amazonaws.com";
- private static final String DATABASE_DRIVER = "com.simba.athena.jdbc.Driver";
+    private static final String DATABASE_DRIVER = "com.simba.athena.jdbc.Driver";
     private static final int SERVER_PORT = 3306;
     static final String USER = "admin";
     static final String PASSWORD = "qwerty123";
@@ -23,10 +23,11 @@ public class UtilAws {
             e.printStackTrace();
         }
     }
+
     public static Connection getConnection() throws SQLException {
         String connectionUrl = "jdbc:mysql://" + DATABASE_URL + ":" + SERVER_PORT + "/" + DATABASE_NAME;
         try {
-                    connection = DriverManager.getConnection(connectionUrl, USER, PASSWORD);
+            connection = DriverManager.getConnection(connectionUrl, USER, PASSWORD);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -35,31 +36,5 @@ public class UtilAws {
         return connection;
     }
 
-
-
-
-    public static void main(String[] args) throws SQLException {
-        Connection connection = getConnection();
-        Statement statement;
-        statement = connection.createStatement();
-    }
-//        String SQL = "CREATE TABLE developers " +
-//                "(id INTEGER not NULL, " +
-//                " name VARCHAR(50), " +
-//                " specialty VARCHAR (50), " +
-//                " salary INTEGER not NULL, " +
-//                " PRIMARY KEY (id))";
-//
-//        statement.executeUpdate(SQL);
-//        if(statement!=null){
-//            statement.close();
-//        }
-//        if(connection!=null){
-//            connection.close();
-//        }
-//    }
-
-
-
-
 }
+
